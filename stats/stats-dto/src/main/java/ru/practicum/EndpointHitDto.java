485eb1a8327f6683d348e73b60b63d052f5efebe
Andrei <An.S-1@yandex.ru>
2024-09-12 10:19:56 +0300

    19

--- stats/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -1,7 +1,11 @@
 package ru.practicum;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Getter
