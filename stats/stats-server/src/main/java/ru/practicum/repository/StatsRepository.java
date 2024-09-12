0ce3c181a2294d315d22ef8a26096404fb7a8ebd
Andrei <An.S-1@yandex.ru>
2024-09-12 11:04:41 +0300

    20

--- stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -3,8 +3,6 @@ package ru.practicum.repository;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import ru.practicum.EndpointHitDto;
-import ru.practicum.ViewStatsDto;
 import ru.practicum.model.EndpointHit;
 import ru.practicum.model.ViewStats;
 
