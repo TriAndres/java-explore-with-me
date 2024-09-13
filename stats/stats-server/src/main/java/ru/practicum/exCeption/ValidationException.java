f7a4dc53bf135eda30efa2db8de1227a18e60dd8
Andrei <An.S-1@yandex.ru>
2024-09-13 11:09:21 +0300

    37

--- stats/stats-server/src/main/java/ru/practicum/exCeption/ValidationException.java
+++ stats/stats-server/src/main/java/ru/practicum/exCeption/ValidationException.java
@@ -1,6 +1,6 @@
 package ru.practicum.exCeption;
 
-public class ValidationException extends RuntimeException{
+public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
     }
