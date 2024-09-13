4f9a8e014571ec63df82b8bfb959de4968659053
Andrei <An.S-1@yandex.ru>
2024-09-13 11:16:10 +0300

    38

---
+++ main-service/src/main/java/ru/practicum/dto/user/UserDto.java
@@ -0,0 +1,15 @@
+package ru.practicum.dto.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private String email;
+    private Long id;
+    private String name;
+}
---
+++ main-service/src/main/java/ru/practicum/dto/user/UserShortDto.java
@@ -0,0 +1,14 @@
+package ru.practicum.dto.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserShortDto {
+
+    private Long id;
+    private String name;
+}
