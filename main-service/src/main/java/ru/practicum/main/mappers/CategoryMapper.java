0b526a275a5d9e1cc48e41400178f47c5aadf4d4
Andrei <An.S-1@yandex.ru>
2024-09-26 13:30:43 +0300

    main_svc 26

--- main-service/src/main/java/ru/practicum/main/mappers/CategoryMapper.java
+++ main-service/src/main/java/ru/practicum/main/mappers/CategoryMapper.java
@@ -1,6 +1,5 @@
 package ru.practicum.main.mappers;
 
-import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 import ru.practicum.main.dto.category.CategoryDto;
 import ru.practicum.main.dto.category.NewCategoryDto;
@@ -8,7 +7,6 @@ import ru.practicum.main.models.Category;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
 @Component
 public interface CategoryMapper {
     Category toCategory(NewCategoryDto newCategoryDto);
