164d9724006fe555d81af7f1066403595e3b1a98
Andrei <An.S-1@yandex.ru>
2024-09-29 16:55:16 +0300

    new

--- main-service/Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM amazoncorretto:21-alpine-jdk
-COPY target/*.jar app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
--- stats/stats-server/src/main/java/ru/practicum/exCeption/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.exCeption;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
