1393789abcbaa8c30846d59af9295e428e9afc51
Andrei <An.S-1@yandex.ru>
2024-10-06 19:25:14 +0300

    6

--- ewm-stats/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ ewm-stats/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StatsServer {
+    
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class, args);
     }
