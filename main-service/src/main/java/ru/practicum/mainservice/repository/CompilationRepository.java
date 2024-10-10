5f821f8944025dc0d7d5557139bf6bf82553f7e6
Andrei <An.S-1@yandex.ru>
2024-10-10 04:15:59 +0300

    ui

--- main-service/src/main/java/ru/practicum/mainservice/repository/CompilationRepository.java
+++ main-service/src/main/java/ru/practicum/mainservice/repository/CompilationRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import ru.practicum.mainservice.model.Compilation;
 
-public interface CompilationRepository extends JpaRepository<Compilation, Integer>,
-        QuerydslPredicateExecutor<Compilation> {
+public interface CompilationRepository extends JpaRepository<Compilation, Integer>, QuerydslPredicateExecutor<Compilation> {
 }
